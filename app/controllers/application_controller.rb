f5537827a8af7b98f338561f70da72ddec090dac
anshulacro <anshul.k@cisinlabs.com>
2015-01-06 14:45:17 +0530

    add a line in application_controller.rb to test git api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+#just to push it to github
