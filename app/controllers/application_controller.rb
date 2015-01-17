3f3a1bc66b87f37310b081e047163b4a89adcec2
anshulacro <anshul.k@cisinlabs.com>
2015-01-17 16:30:24 +0530

    add a line to test git hub api calling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Add another line to controllers to make a commit to test github api callings.
   protect_from_forgery with: :exception
 end
